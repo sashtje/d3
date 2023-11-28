d2cd92131df8b1049505d284e3510d4944c2115c
sashtje <sashtje.lefevre@gmail.com>
2023-11-28 16:22:11 +0300

    feat: add axes

--- scatterplot/scripts.js
+++ scatterplot/scripts.js
@@ -10,7 +10,44 @@ async function getJSONData() {
   }
 }
 
-async function createChart(data) {}
+async function createChart(data) {
+  const svg = d3.select(".scatterplot__chart");
+  const w = svg.attr("width");
+  const h = svg.attr("height");
+  const padding = 40;
+
+  const minDate = new Date(d3.min(data, (d) => d.Year - 1).toString());
+  const maxDate = new Date(d3.max(data, (d) => d.Year + 1).toString());
+
+  const xScale = d3
+    .scaleTime()
+    .domain([minDate, maxDate])
+    .range([padding, w - padding]);
+  const xAxis = d3.axisBottom(xScale);
+  svg
+    .append("g")
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0, " + (h - padding) + ")")
+    .call(xAxis);
+
+  data.forEach((item) => {
+    const [minutes, seconds] = item.Time.split(":");
+    item.Time = new Date(1970, 0, 1, 0, minutes, seconds);
+  });
+  const minTime = d3.min(data, (d) => d.Time);
+  const maxTime = d3.max(data, (d) => d.Time);
+
+  const yScale = d3
+    .scaleTime()
+    .domain([minTime, maxTime])
+    .range([padding, h - padding]);
+  const yAxis = d3.axisLeft(yScale);
+  svg
+    .append("g")
+    .attr("id", "y-axis")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(yAxis.tickFormat(d3.timeFormat("%M:%S")));
+}
 
 async function main() {
   const data = await getJSONData();
